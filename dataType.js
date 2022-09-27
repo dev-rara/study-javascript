2e9e9542c048c14bd7e1c03d3d178ddcc60bcab3
rara-dev <kksr1219@gmail.com>
2022-09-27 21:59:25 +0900

    first commit

---
+++ dataType.js
@@ -0,0 +1,14 @@
+ console.log(100)
+
+ let age = 33
+ console.log(age)
+
+ let name = 'Rara Kim'
+ console.log(name)
+
+ let isMinus = true
+ let isOk = false
+ console.log(isMinus)
+ console.log(isOk)
+
+ 
---
+++ variable.js
@@ -0,0 +1,11 @@
+// let myName = 'Sora Kim'
+// console.log(myName)
+
+// myName = 'Jenny'
+// console.log(myName)
+
+
+const name = 'Sora Kim'
+console.log(name)
+
+// name = 'Jenny'
