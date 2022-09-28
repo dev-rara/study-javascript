169af8a4615003f61e4905cf6f0ad4e9472c192f
rara-dev <kksr1219@gmail.com>
2022-09-28 23:31:55 +0900

    study for/if

---
+++ for_if.js
@@ -0,0 +1,14 @@
+for (let number = 1; number <= 10; number++) {
+    if (number % 3 === 0) {
+        console.log(`${number}는 3으로 나누어 떨어지는 숫자입니다.`)
+    }
+}
+
+
+for (let num = 1; num <= 20; num++) {
+    if (num % 2 === 0) {
+        console.log(`${num}은 짝수 입니다.`)
+    } else {
+        console.log(`${num}은 홀수 입니다.`)
+    }
+}
