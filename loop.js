ccd617bdfddb7449777bc27a52c4522b7fce4e64
rara-dev <kksr1219@gmail.com>
2022-09-28 23:23:09 +0900

    study while/for

---
+++ loop.js
@@ -0,0 +1,10 @@
+let temperature = 20
+while (temperature < 25) {
+    console.log(`${temperature}도 정도면 적당한 온도입니다.`)
+    temperature++
+}
+
+
+for (let temp = 20; temp < 25; temp++) {
+    console.log(`${temp}도 정도면 적당한 온도입니다.`)
+}
