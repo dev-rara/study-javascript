fbe21e347604cb115de15832089c6e078529262e
rara-dev <kksr1219@gmail.com>
2022-09-28 22:56:51 +0900

    study if-else if-else

---
+++ if.js
@@ -0,0 +1,14 @@
+const shoesPrice = 40000
+if(shoesPrice < 50000) {
+    console.log('이 신발을 사겠습니다.')
+} 
+
+const capPrice = 40000
+if(capPrice < 40000) {
+    console.log('이 모자를 사겠습니다.')
+} else if(capPrice <= 40000) {
+    console.log('고민 좀 해볼게요...')
+} else {
+    console.log('너무 비싸네요!')
+}
+
